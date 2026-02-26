6a11a60f90e1ba3782b09a6c050fba54f75a2ecb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-26 11:42:41 -0300

    Aula02

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,28 +1,31 @@
 package br.com.fiapride.main;
 
-
-import br.com.fiapride.model.Geladeira;
+// Importamos a classe Passageiro para que o sistema a reconheça
+import br.com.fiapride.model.Passageiro;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
-        Geladeira MinhaGeladeira = new Geladeira ();
-        MinhaGeladeira.cor = "Cinza";
-        MinhaGeladeira.material = "Aço";
-        MinhaGeladeira.potencia = 400;
+        // INSTANCIAÇÃO
+        // O comando 'new' aloca memória para um novo objeto.
+        // Criando o primeiro passageiro (Objeto 1)
+        Passageiro passageiro1 = new Passageiro("Ana Silva", "222");
+        System.out.println("Regarga passageiro 1");
+        passageiro1.adicionarSaldo(50.0);
+
+        // Criando o segundo passageiro (Objeto 2)
+        Passageiro passageiro2 = new Passageiro("Carlos Souza", "333");
+        System.out.println("Regarga passageiro 2");
+        passageiro2.adicionarSaldo(12.5);
+
+        System.out.println("--- Sistema FiapRide ---");
+        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$ " + passageiro1.saldo + " | CPF: " + passageiro1.cpf);
+        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$ " + passageiro2.saldo + " | CPF: " + passageiro2.cpf);
         
-        Geladeira ProfessorGeladeira = new Geladeira();
-        ProfessorGeladeira.cor = "Branca";
-        ProfessorGeladeira.material = "Aço";
-        ProfessorGeladeira.potencia = 500;
-        
-        System.out.println("Minha geladeira é da cor:"+ MinhaGeladeira.cor);
-        System.out.println("Minha geladeira é feita de:"+ MinhaGeladeira.material);
-        System.out.println("A potência da minha geladeira é:"+ MinhaGeladeira.potencia);
-        
-        System.out.println("A geladeira do professor é da cor:"+ ProfessorGeladeira.cor);
-        System.out.println("A geladeira é feita de:"+ ProfessorGeladeira.material);
-        System.out.println("A potência da geladeira do professor é:"+ ProfessorGeladeira.potencia);
-        
+        System.out.println("Pagando viagem do passageiro 1");
+        passageiro1.pagarViagem(20);
+        System.out.println("Pagando viagem do passageiro 2");
+        passageiro2.pagarViagem(20);
+
     }
 }
